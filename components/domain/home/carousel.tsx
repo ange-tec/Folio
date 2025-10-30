1100ab49630fb3379f51732b0ad85aee45c9297c
ange-tec <angemoudzouma@gmail.com>
2025-10-30 17:12:40 +0100

    fix/component

--- components/domain/home/carousel.tsx
+++ components/domain/home/carousel.tsx
@@ -10,34 +10,47 @@ import {
 } from "@/components/ui/carousel"
 
 
-const data = [
-    {
-        title: "Llkdo",
-        description: "A website project to help entreprises to offer some gifts employees",
-        image: "",
-     },
-    {
-        intro: "Featured project",
-        title: "Projet 2",
-        description: "Voici mon premier projet"
-    }]
+const data =
+    [
+        {
+            title: "Llkdo",
+            description: "A website project to help entreprises to offer some gifts employees",
+            image: "",
+         },
+        {
+            title: "Anticimex.shop",
+            description: "Voici mon premier projet",
+            image: ""
+        },
+        {
+            title: "Pg Particulier",
+            description: "Voici mon premier projet",
+            image: ""
+        }
+    ]
 
 export  default function CarouselProjects() {
     return (
-        <Carousel className="w-full max-w-xs">
+        <Carousel className="w-full max-w-xl">
             <CarouselContent>
                 {data.map((projet, index) => (
                     <CarouselItem key={index}>
-                            <Card>
-                                <CardContent className="aspect-square items-center justify-center">
-                                    <div className="grid grid-cols-2 gap-4">
-                                        <div className="bg-red-200"> 1 </div>
-                                        <div className="bg-amber-600">
-                                            {projet.image}
-                                        </div>
+                        <Card>
+                            <CardContent className="aspect-square items-center justify-center">
+                                <div className="flex flex-col gap-10">
+                                    <div className="bg-red-200">
+                                        <h3>{projet.title} </h3>
+                                        <p>{projet.description}</p>
                                     </div>
-                                </CardContent>
-                            </Card>
+                                    <div className="bg-amber-600">
+                                        {projet.image}
+                                    </div>
+                                    <div className="bg-amber-200">
+                                        Les technos
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
                     </CarouselItem>
                 ))}
             </CarouselContent>
--- components/domain/home/homelist-second.tsx
+++ components/domain/home/homelist-second.tsx
@@ -2,15 +2,15 @@ import CarouselProjects from "@/components/domain/home/carousel";
 
 export default function HomelistSecond () {
     return (
-        <section className="w-auto">
-            <div className="d-flex flex-row align-items-center">
+        <section className="w-auto flex flex-col gap-4">
+            <div className="w-auto flex justify-center items-center gap-8">
                 <div className="">
                     <span> Projects </span>
                     Latest Projects
                 </div>
                 <button className="bg-blue-600"> View all </button>
             </div>
-            <div className="">
+            <div className="w-auto flex justify-center items-center gap-6">
                 <CarouselProjects />
             </div>
         </section>
